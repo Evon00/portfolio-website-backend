fe0729c2ff74414210d1148ce5811c3cffd3e934
Evon00 <dbwogur36@gmail.com>
2025-08-04 23:59:47 +0900

    Initial commit

---
+++ src/test/java/com/example/portfolio_website_backend/PortfolioWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfolio_website_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
