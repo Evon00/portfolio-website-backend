e388b14c0decb2ef0fd54ad1c5f9825b0e17d154
Evon00 <dbwogur36@gmail.com>
2025-08-06 16:51:00 +0900

    [feat] ProjectImage 테이블 컬럼명 수정

--- src/main/java/com/example/portfolio_website_backend/project/domain/ProjectImage.java
+++ src/main/java/com/example/portfolio_website_backend/project/domain/ProjectImage.java
@@ -1,24 +1,26 @@
 package com.example.portfolio_website_backend.project.domain;
 
 import com.example.portfolio_website_backend.common.domain.ImageContent;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
+@SuperBuilder
 public class ProjectImage extends ImageContent {
 
     @ManyToOne
     @JoinColumn(name = "project_id")
     private Project project;
 
-    private int order;
+    @Column(name = "display_order")
+    private int displayOrder;
+
 }
